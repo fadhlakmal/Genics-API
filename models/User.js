ba2d98b7f0413086b818c26031873b63b07fac08
Fadhl Akmal <120249194+fadhlakmal@users.noreply.github.com>
2023-06-20 19:31:49 +0700

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const {
+  Schema,
+  model
+} = require("mongoose");
+
+// Create schema
+// - name: String
+// - age: Number
+
+const User = Schema({
+  name:{
+    type: String,
+    required: true
+  },
+  age:{
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = model('Users', User);
